dc33c3649c326a9feed3e36037f4dd70308dc88c
nikfed1 <nikfed1@gmail.com>
2017-02-26 13:43:47 +0300

    ok background/fields improvement

--- js/engine/fields.js
+++ js/engine/fields.js
@@ -411,7 +411,6 @@ CDIFieldChunk = createClass
 	                }
 	                if (this._adChunks[this._nActiveChunk].isFull()) {
 	                    var nNextChunk = this._getFirstNoFullChunk();
-
 	                    if (nNextChunk > -1) {
 	                        if (!$isNoU(this._aChunks[nPrevIdx]) && this._aChunks[nPrevIdx].type == "STATIC") {
 	                            $(this._aChunks[nPrevIdx].place).innerHTML =
@@ -573,6 +572,7 @@ CDIFieldChunk = createClass
 	        }
 	        this._bStrip = $bool(oField["strip"]);
 	        this._sName = "";
+			this.lastActionBackSpace = false;
 	        this._sType = "";
 	        this._sDesc = "";
 	        this._sTypeField = "";
@@ -1011,6 +1011,7 @@ CDIFieldChunk = createClass
 
 	        switch (sValue) {
 	            case "BACKSPACE":
+					this.lastActionBackSpace = true;
 	                if (this.isEmpty()) {
 	                    this._nCurrentBlock = 0;
 	                    this._aBlocks[0].setActive(true);
@@ -1032,7 +1033,17 @@ CDIFieldChunk = createClass
 	                }
 	                break;
 	            default:
-
+					
+					if(this.lastActionBackSpace && this._aBlocks[this._nCurrentBlock].isFull()) {
+						var nBlock = this._getNextNoFullBlock();
+						if (nBlock == -1) {
+							nBlock = this._getLastNoEmptyBlock();
+						}
+						this._aBlocks[this._nCurrentBlock].setActive(false);
+						this._nCurrentBlock = nBlock;
+						this.lastActionBackSpace = false;
+						this._aBlocks[nBlock].setActive(true);
+					}
 	                this._aBlocks[this._nCurrentBlock].processValue(sValue);
 
 	        }
